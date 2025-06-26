6221f9d0300d53020e0bd5f0bda093668d5bfd7d
nagarjunadeveloper <kodurinagarjuna1@gmail.com>
2025-06-26 15:15:41 +0530

    day3 commit

--- day2/main.tf
+++ day2/main.tf
@@ -1,11 +1,16 @@
-resource "aws_instance" "name" {
+# resource "aws_instance" "name" {
   
-  ami="ami-02457590d33d576c3"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-0a6964b7243955121"
+#   ami="ami-02457590d33d576c3"
+#   instance_type = "t2.micro"
+#   subnet_id = "subnet-0a6964b7243955121"
 
-  tags = {
-    Name="nag"
-  }
+#   tags = {
+    # Name="nag"
+#   }
+# }
+
+resource "aws_s3_bucket" "uniquebucket" {
+  
+  bucket="terraformnagstorage"
 }
 
